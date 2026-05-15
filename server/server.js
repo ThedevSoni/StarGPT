b6a6b17fe396abe4ae95c641e25400ff7a3cd3ab
ThedevSoni <135100873+ThedevSoni@users.noreply.github.com>
2026-05-15 09:38:48 +0530

    Add global API rate limiting middleware to server

--- server/server.js
+++ server/server.js
@@ -7,18 +7,21 @@ import chatRouter from "./routes/chatRoutes.js"
 import messageRouter from "./routes/messageRoutes.js"
 import creditRouter from "./routes/creditRouter.js"
 import { stripeWebhooks } from "./controllers/webhooks.js"
-
-
+import { apiLimiter } from "./middlewares/rateLimit.js"
 
 const app = express()
 
 await connectDB()
+
 //Stipe webhooks
 app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// Apply global rate limiter to API routes
+app.use('/api/', apiLimiter)
 
 // Routes
 app.get('/', (req, res) => res.send('server is Live!'))
