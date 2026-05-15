6f7667f4ee5fd565824a9d8d06eddef7393a9334
ThedevSoni <135100873+ThedevSoni@users.noreply.github.com>
2026-05-15 09:28:11 +0530

    Apply rate limiting to chat create endpoint

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createChat, getChats, deleteChat } from '../controllers/chatController.js';
 import { protect } from '../middlewares/auth.js';
+import { chatLimiter } from '../middlewares/rateLimit.js';
 
 const chatRouter = express.Router();
 
-chatRouter.get('/create', protect, createChat);
+chatRouter.get('/create', protect, chatLimiter, createChat);
 chatRouter.get('/get', protect, getChats);
 chatRouter.post('/delete', protect, deleteChat);
 
