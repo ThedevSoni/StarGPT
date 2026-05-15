7ae3481e1bb8f7257325793ac4c53d4a3ad3e376
ThedevSoni <135100873+ThedevSoni@users.noreply.github.com>
2026-05-15 09:37:46 +0530

    Apply rate limiting middleware to message routes to prevent 429 errors

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { imageMessageController, textMessageController } from '../controllers/messageController.js';
 import { protect } from '../middlewares/auth.js';
-const messageRouter =express.Router()
-messageRouter.post('/text' , protect , textMessageController)
-messageRouter.post('/image' , protect , imageMessageController)
+import { apiLimiter } from '../middlewares/rateLimit.js';
+
+const messageRouter = express.Router()
+
+messageRouter.post('/text', protect, apiLimiter, textMessageController)
+messageRouter.post('/image', protect, apiLimiter, imageMessageController)
+
 export default messageRouter
